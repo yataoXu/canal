f6c8a8c2214c7775c8a144e2df102785c32b708d
evan <xuyt1992@163.com>
2019-10-12 23:54:51 +0800

    spring 集成 canal kafka

---
+++ canal/src/main/java/com/evan/canal/service/MessageService.java
@@ -0,0 +1,33 @@
+package com.evan.canal.service;
+
+import com.evan.canal.constants.KafkaConsts;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description 消息处理
+ * @ClassName MessageServuce
+ * @Author Evan
+ * @date 2019.10.12 23:23
+ */
+
+@Component
+@Slf4j
+public class MessageService {
+
+    @KafkaListener(topics = KafkaConsts.TOPIC_TEST, containerFactory = "ackContainerFactory")
+    public void handleMessage(ConsumerRecord record, Acknowledgment acknowledgment) {
+        try {
+            String message = (String) record.value();
+            log.info("收到消息: {}", message);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            // 手动提交 offset
+            acknowledgment.acknowledge();
+        }
+    }
+}
