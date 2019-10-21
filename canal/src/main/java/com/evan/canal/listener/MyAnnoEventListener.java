5741dcee19dcd2052c36ace4fbdc4e1e6f0779cb
evan <xuyt1992@163.com>
2019-10-21 17:34:23 +0800

    cannal 接收数据

--- canal/src/main/java/com/evan/canal/listener/MyAnnoEventListener.java
+++ canal/src/main/java/com/evan/canal/listener/MyAnnoEventListener.java
@@ -1,5 +1,7 @@
 package com.evan.canal.listener;
 
+import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.DateUtil;
 import com.alibaba.otter.canal.protocol.CanalEntry;
 import com.evan.canal.annotation.CanalEventListener;
 import com.evan.canal.annotation.ddl.AlertTableListenPoint;
@@ -10,8 +12,15 @@ import com.evan.canal.annotation.dml.DeleteListenPoint;
 import com.evan.canal.annotation.dml.InsertListenPoint;
 import com.evan.canal.annotation.dml.UpdateListenPoint;
 import com.evan.canal.core.CanalMsg;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.CollectionUtils;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -21,81 +30,65 @@ import java.util.List;
  * @date 2019.10.17 11:28
  */
 @CanalEventListener
+@Slf4j
 public class MyAnnoEventListener {
+
+    @Value("${access_log_dir}")
+    private final String ACCESS_LOG_DIR = "D:/hadoop/mysql";
 
     @InsertListenPoint
     public void onEventInsertData(CanalMsg canalMsg, CanalEntry.RowChange rowChange) {
-        System.out.println("==注解方式（新增数据操作）=======");
-        List<CanalEntry.RowData> rowDatasList = rowChange.getRowDatasList();
-        for (CanalEntry.RowData rowData : rowDatasList) {
-            String sql = "use " + canalMsg.getSchemaName() + ";\n";
-            StringBuffer colums = new StringBuffer();
-            StringBuffer values = new StringBuffer();
-            rowData.getAfterColumnsList().forEach((c) -> {
-                colums.append(c.getName() + ",");
-                values.append(c.getValue() + "\t");
-            });
-
-
-//            sql += "INSERT INTO " + canalMsg.getTableName() + "(" + colums.substring(0, colums.length() - 1) + ") VALUES(" + values.substring(0, values.length() - 1) + ");";
-//            System.out.println(sql);
-            System.out.println(values.substring(0, values.length() - 1));
-        }
-        System.out.println("====================");
-
+        log.info("注解方式---新增数据操作");
+        InsertOrUpdateDate(rowChange, canalMsg);
     }
+
 
     @UpdateListenPoint
     public void onEventUpdateData(CanalMsg canalMsg, CanalEntry.RowChange rowChange) {
-        System.out.println("=====注解方式（更新数据操作）=========");
-        List<CanalEntry.RowData> rowDatasList = rowChange.getRowDatasList();
-        for (CanalEntry.RowData rowData : rowDatasList) {
-
-            String sql = "use " + canalMsg.getSchemaName() + ";\n";
-            StringBuffer updates = new StringBuffer();
-            StringBuffer conditions = new StringBuffer();
-            rowData.getAfterColumnsList().forEach((c) -> {
-                if (c.getIsKey()) {
-                    conditions.append(c.getName() + "='" + c.getValue() + "'");
-                } else {
-                    updates.append(c.getName() + "='" + c.getValue() + "',");
-                }
-            });
-            sql += "UPDATE " + canalMsg.getTableName() + " SET " + updates.substring(0, updates.length() - 1) + " WHERE " + conditions;
-            System.out.println(sql);
-        }
-        System.out.println("\n==========");
+        log.info("注解方式---更新数据操作");
+        InsertOrUpdateDate(rowChange, canalMsg);
     }
 
     @DeleteListenPoint
     public void onEventDeleteData(CanalEntry.RowChange rowChange, CanalMsg canalMsg) {
 
-        System.out.println("====注解方式（删除数据操作）========");
+        log.info("注解方式---删除数据操作");
+        DeleteData(rowChange, canalMsg);
+
+
+    }
+
+    public void DeleteData(CanalEntry.RowChange rowChange, CanalMsg canalMsg) {
         List<CanalEntry.RowData> rowDatasList = rowChange.getRowDatasList();
         for (CanalEntry.RowData rowData : rowDatasList) {
-
             if (!CollectionUtils.isEmpty(rowData.getBeforeColumnsList())) {
-                String sql = "use " + canalMsg.getSchemaName() + ";\n";
-
-                sql += "DELETE FROM " + canalMsg.getTableName() + " WHERE ";
-                StringBuffer idKey = new StringBuffer();
-                StringBuffer idValue = new StringBuffer();
-                for (CanalEntry.Column c : rowData.getBeforeColumnsList()) {
-                    if (c.getIsKey()) {
-                        idKey.append(c.getName());
-                        idValue.append(c.getValue());
-                        break;
-                    }
-
-
-                }
-
-                sql += idKey + " =" + idValue + ";";
-                System.out.println(sql);
+                StringBuffer values = new StringBuffer();
+                rowData.getBeforeColumnsList().forEach((c) -> {
+                    values.append(c.getValue()).append("\t");
+                });
+                initWriteFile(values, rowChange, canalMsg);
             }
-            System.out.println("\n===============");
-
         }
+    }
+
+    public void InsertOrUpdateDate(CanalEntry.RowChange rowChange, CanalMsg canalMsg) {
+        List<CanalEntry.RowData> rowDatasList = rowChange.getRowDatasList();
+        for (CanalEntry.RowData rowData : rowDatasList) {
+            StringBuffer values = new StringBuffer();
+            rowData.getAfterColumnsList().forEach((c) -> {
+                values.append(c.getValue()).append("\t");
+            });
+            initWriteFile(values, rowChange, canalMsg);
+        }
+
+    }
+
+
+    private void initWriteFile(StringBuffer values, CanalEntry.RowChange rowChange, CanalMsg canalMsg) {
+        Date date = DateUtil.date();
+        String dateStr = DateUtil.format(date, DatePattern.NORM_DATETIME_MS_PATTERN);
+        String columnValue = values.append(rowChange.getEventType()).append("\t").append(dateStr).append("\n").toString();
+        writeFile(canalMsg.getSchemaName(), canalMsg.getTableName() + "_" + rowChange.getEventType().toString().toLowerCase(), columnValue);
     }
 
     @CreateTableListenPoint
@@ -120,11 +113,57 @@ public class MyAnnoEventListener {
     }
 
     @CreateIndexListenPoint
-    public void onEventCreateIndex(CanalMsg canalMsg,CanalEntry.RowChange rowChange){
+    public void onEventCreateIndex(CanalMsg canalMsg, CanalEntry.RowChange rowChange) {
         System.out.println("======================注解方式（创建索引操作）==========================");
-        System.out.println("use " + canalMsg.getSchemaName()+ ";\n" + rowChange.getSql());
+        System.out.println("use " + canalMsg.getSchemaName() + ";\n" + rowChange.getSql());
         System.out.println("\n======================================================");
 
+    }
+
+
+    public void writeFile(String databaseName, String tableName, String content) {
+
+        FileWriter fw = null;
+        BufferedWriter bw = null;
+        try {
+            File srcDir = srcDirFolder(databaseName, tableName);
+            fw = new FileWriter(srcDir, true);
+            bw = new BufferedWriter(fw);
+            fw.write(content);
+            fw.flush();
+            fw.close();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != fw) {
+                    fw.close();
+                }
+                if (null != bw) {
+                    bw.close();
+                }
+                bw = null;
+                fw = null;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private File srcDirFolder(String databaseName, String tableName) throws IOException {
+
+        String now = DateUtil.today();
+        File srcDirFile = new File(ACCESS_LOG_DIR + "/" + databaseName + "/" + tableName + "/" + tableName + now);
+        if (!srcDirFile.getParentFile().exists()) {
+            boolean mkdirs = srcDirFile.getParentFile().mkdirs();
+            if (!mkdirs) {
+                log.error("{}父文件夹创建失败", srcDirFile);
+                throw new RuntimeException("父文件夹创建失败");
+            }
+        }
+        srcDirFile.createNewFile();
+        return srcDirFile;
     }
 
 
