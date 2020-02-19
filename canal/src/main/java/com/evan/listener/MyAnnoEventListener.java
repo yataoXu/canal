bc15681469cedb43995a9458c0e41cf9fc455d82
xu.yatao <Xuyt1992@163.com>
2020-02-19 22:52:39 +0800

    vvvv

--- canal/src/main/java/com/evan/listener/MyAnnoEventListener.java
+++ canal/src/main/java/com/evan/listener/MyAnnoEventListener.java
@@ -25,7 +25,6 @@ import java.util.Map;
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class MyAnnoEventListener {
 
-
     private CreateSqlService createSqlService;
 
     @InsertListenPoint
@@ -47,21 +46,37 @@ public class MyAnnoEventListener {
         log.info("删除数据操作");
         String eventType = rowChange.getEventType().toString();
         List<CanalEntry.RowData> rowDatasList = rowChange.getRowDatasList();
+        boolean flage = true;
         for (CanalEntry.RowData rowData : rowDatasList) {
             String schemaName = canalMsg.getSchemaName();
             String tableName = canalMsg.getTableName();
+            StringBuffer columnName = new StringBuffer();
             StringBuffer values = new StringBuffer();
-            rowData.getBeforeColumnsList().forEach((c) -> {
-                values.append(c.getValue() + "\t");
-            });
-            writeDatAdd(values, eventType, schemaName, tableName);
+            Map<String, String> kv = MapUtil.newHashMap();
 
+            for (CanalEntry.Column column : rowData.getBeforeColumnsList()) {
+                boolean isKey = column.getIsKey();
+                if (isKey) {
+                    kv.put("key", column.getName());
+                    kv.put("value", column.getValue());
+                }
+                columnName.append(column.getName() + "\t");
+                values.append(column.getValue() + "\t");
+            }
+
+            writeData(values.toString(), eventType, schemaName, tableName);
+
+            if (flage) {
+                String deleteSql = createSqlService.createDeleteSql(canalMsg, kv, columnName.toString());
+                writeData(deleteSql, eventType + "Sql", schemaName, schemaName);
+                flage = false;
+            }
         }
     }
 
-    private void writeDatAdd(StringBuffer values, String eventType, String schemaName, String tableName) {
-        log.info("库名：{},表名：{}，操作类型：{},数据：{}", schemaName, tableName, eventType, values);
-        FileUtils.writeFile(eventType, schemaName, tableName, values.toString());
+    private void writeData(String content, String eventType, String schemaName, String tableName) {
+        log.info("库名：{},表名：{}，操作类型：{},数据：{}", schemaName, tableName, eventType, content);
+        FileUtils.writeFile(eventType, schemaName, tableName, content);
     }
 
 
@@ -78,8 +93,9 @@ public class MyAnnoEventListener {
                 values.append(c.getValue() + "\t");
             });
 
-            createSqlService.createInsertSql(canalMsg, values.toString());
-            writeDatAdd(values, eventType, schemaName, tableName);
+            String insertSql = createSqlService.createInsertSql(canalMsg, values.toString());
+            writeData(insertSql,eventType + "Sql", schemaName, tableName);
+            writeData(values.toString(), eventType, schemaName, tableName);
         }
     }
 
@@ -88,6 +104,7 @@ public class MyAnnoEventListener {
         String eventType = rowChange.getEventType().toString();
         List<CanalEntry.RowData> rowDatasList = rowChange.getRowDatasList();
 
+        boolean flage = true;
         for (CanalEntry.RowData rowData : rowDatasList) {
             String schemaName = canalMsg.getSchemaName();
             String tableName = canalMsg.getTableName();
@@ -103,8 +120,12 @@ public class MyAnnoEventListener {
                 columnName.append(column.getName() + "\t");
                 values.append(column.getValue() + "\t");
             }
-            createSqlService.createUpdateSql(canalMsg, kv, columnName.toString());
-            writeDatAdd(values, eventType, schemaName, tableName);
+            if (flage) {
+                String updateSql = createSqlService.createUpdateSql(canalMsg, kv, columnName.toString());
+                writeData(updateSql, eventType + "Sql", schemaName, schemaName);
+                flage = false;
+            }
+            writeData(values.toString(), eventType, schemaName, tableName);
         }
     }
 
--- canal/src/main/java/com/evan/service/CreateSqlService.java
+++ canal/src/main/java/com/evan/service/CreateSqlService.java
@@ -1,10 +1,15 @@
 package com.evan.service;
 
+import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
 import com.evan.core.CanalMsg;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.unit.DataUnit;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -15,64 +20,74 @@ import java.util.stream.Collectors;
  * @Date 2020/2/19 16:08
  * @Created by Evan
  */
-@Slf4j
-@Service
+//@Slf4j
+//@Service
 public class CreateSqlService {
 
 
-    public String createInsertSql(CanalMsg canalMsg, String values){
+    //insert overwrite  table ods_haha select * from haha h where h.dt ='20200218' UNION SELECT * from haha_insert hi  where hi.dt ='20200218'
+    public String createInsertSql(CanalMsg canalMsg, String columnName) {
+        DateTime yesterday = DateUtil.yesterday();
+        DateTime now = DateTime.now();
+
         StringBuffer sql = new StringBuffer();
-        List<String> collect = Arrays.stream(values.split("\t")).map((s) -> s+",").collect(Collectors.toList());
-        sql.append("insert into ").append(canalMsg.getTableName()).append(" values(");
+        List<String> collect = Arrays.stream(columnName.split("\t")).map((s) -> s + ",").collect(Collectors.toList());
+
+        sql.append("insert overwrite table ").append(canalMsg.getSchemaName() + "." + canalMsg.getTableName()).append(" select ");
         collect.forEach(sql::append);
         sql = sql.deleteCharAt(sql.length() - 1);
-        sql.append(");\n");
+        sql.append(" from ").append(canalMsg.getSchemaName() + "." + canalMsg.getTableName()).append(" h where h.dt='").append(yesterday.toString(DatePattern.PURE_DATE_PATTERN))
+                .append("'  UNION SELECT ");
+        collect.forEach(sql::append);
+        sql = sql.deleteCharAt(sql.length() - 1);
+        sql.append(" from ").append(canalMsg.getSchemaName() + "." + canalMsg.getTableName())
+                .append("_insert hi.dt='").append( now.toString(DatePattern.PURE_DATE_PATTERN)).append("';\n");
         System.out.println(sql);
         return sql.toString();
     }
 
     //insert overwrite  table ods_haha select  * from (SELECT h.id,h.name FROM student.ods_haha h LEFT JOIN student.haha_update hu on h.id = hu.id where hu.id is null  UNION select id,name from haha_update) as aa;
-    public String createUpdateSql(CanalMsg canalMsg, Map<String,String> kv, String columnName){
+    public String createUpdateSql(CanalMsg canalMsg, Map<String, String> kv, String columnName) {
         StringBuffer sql = new StringBuffer();
-        List<String> columnList = Arrays.stream(columnName.split("\t")).map((s) -> s+",").collect(Collectors.toList());
+        List<String> columnList = Arrays.stream(columnName.split("\t")).map((s) -> s + ",").collect(Collectors.toList());
 
-        sql.append("insert overwrite ").append(canalMsg.getSchemaName()+"."+canalMsg.getTableName()).append(" select * from (SELECT ");
-        for(String column :columnList){
+        sql.append("insert overwrite table ").append(canalMsg.getSchemaName() + "." + canalMsg.getTableName()).append(" select * from (SELECT ");
+        for (String column : columnList) {
             sql.append("h_").append(column);
         }
         sql = sql.deleteCharAt(sql.length() - 1);
-        sql.append(" FROM ").append(canalMsg.getSchemaName()+"."+canalMsg.getTableName()).append(" h ").append("LEFT JOIN ")
-                .append(canalMsg.getSchemaName()+"."+canalMsg.getTableName()).append("_update hu").append(" on h.").append(kv.get("key"))
+        sql.append(" FROM ").append(canalMsg.getSchemaName() + "." + canalMsg.getTableName()).append(" h ").append("LEFT JOIN ")
+                .append(canalMsg.getSchemaName() + "." + canalMsg.getTableName()).append("_update hu").append(" on h.").append(kv.get("key"))
                 //h.id = hu.id where hu.id is null
-        .append(" = hu.").append(kv.get("key")).append(" where hu.").append(kv.get("key")).append(" is null").append(" UNION select ");
-                //  id,name from haha_update) as aa;
-        for(String column :columnList){
+                .append(" = hu.").append(kv.get("key")).append(" where hu.").append(kv.get("key")).append(" is null").append(" UNION select ");
+        //  id,name from haha_update) as aa;
+        for (String column : columnList) {
             sql.append(column);
         }
         sql = sql.deleteCharAt(sql.length() - 1);
-        sql.append(" from ").append(canalMsg.getSchemaName()+"."+canalMsg.getTableName()).append("_update").append(") as aa;\n");
+        sql.append(" from ").append(canalMsg.getSchemaName() + "." + canalMsg.getTableName()).append("_update").append(") as aa;\n");
         System.out.println(sql);
         return sql.toString();
     }
 
     //insert overwrite  table ods_haha SELECT h.id,h.name FROM student.ods_haha h LEFT JOIN student.haha_delete hu on h.id = hu.id where hu.id is null;
-    public String createDeleteSql(CanalMsg canalMsg, Map<String,String> kv, String columnName) {
+    public String createDeleteSql(CanalMsg canalMsg, Map<String, String> kv, String columnName) {
         StringBuffer sql = new StringBuffer();
-        List<String> columnList = Arrays.stream(columnName.split("\t")).map((s) -> s+",").collect(Collectors.toList());
+        List<String> columnList = Arrays.stream(columnName.split("\t")).map((s) -> s + ",").collect(Collectors.toList());
 
-        sql.append("insert overwrite ").append(canalMsg.getSchemaName()+"."+canalMsg.getTableName()).append(" select  * from  (SELECT ");
-        for(String column :columnList){
+        sql.append("insert overwrite table ").append(canalMsg.getSchemaName() + "." + canalMsg.getTableName()).append(" select  * from  (SELECT ");
+        for (String column : columnList) {
             sql.append("h_").append(column);
         }
         sql = sql.deleteCharAt(sql.length() - 1);
-        sql.append("FROM").append(canalMsg.getSchemaName()+"."+canalMsg.getTableName()).append(" h ").append("LEFT JOIN")
-                .append(canalMsg.getSchemaName()+"."+canalMsg.getTableName()).append("_delete hu ").append(" on h.").append(kv.get("key"))
-                .append(" = hu.").append(kv.get("key")).append("  where hu.").append(kv.get("key")).append(" is null;\n");
+        sql.append("FROM").append(canalMsg.getSchemaName() + "." + canalMsg.getTableName()).append(" h ").append("LEFT JOIN")
+                .append(canalMsg.getSchemaName() + "." + canalMsg.getTableName()).append("_delete hu ").append(" on h.").append(kv.get("key"))
+                .append(" = hu.").append(kv.get("key")).append(" where hu.").append(kv.get("key")).append(" is null;\n");
         System.out.println(sql);
         return sql.toString();
     }
 
-//    public static void main(String[] args) {
+    public static void main(String[] args) {
 //        String s = "ss\tdd\tddd\tddsdf\tdsfsdf\tsdfsdf\tsdfsdf\t";
 //        CanalMsg canalMsg = new CanalMsg("dsfsdf","sdfsd","ewrwer");
 //        Map map = MapUtil.newHashMap();
@@ -80,7 +95,15 @@ public class CreateSqlService {
 //        map.put("value","value");
 //        CreateSqlService createSqlService = new CreateSqlService();
 //        createSqlService.createUpdateSql(canalMsg,map,s);
-//
-//    }
+
+        DateTime yesterday = DateUtil.yesterday();
+        DateTime now = DateTime.now();
+
+        System.out.println(now.toString(DatePattern.PURE_DATE_PATTERN));
+
+        System.out.println(yesterday.toString(DatePattern.PURE_DATE_PATTERN));
+
+
+    }
 
 }
